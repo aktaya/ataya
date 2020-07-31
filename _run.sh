6ea8d6d33f9197b901d8d7597bbf14fcdd5b9dbb
exyrias <25100303+exyrias@users.noreply.github.com>
2020-07-31 15:22:01 +0900

    implement: list TODO when execute _run.sh

--- _run.sh
+++ _run.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+
+function todo(){
+  str="$1"
+  echo
+  echo -n "  "
+  echo -en "\033[48;2;255;0;0m"
+  echo -en "\033[38;2;0;0;0m"
+  echo -en "\033[1m"
+  echo -n "TODO:"
+  echo -en "\033[0m"
+  echo -n " "
+  echo "$1"
+  echo
+}
+
+todo "page num., english of tech. reports"
+
 bundle exec jekyll serve -H 0.0.0.0
